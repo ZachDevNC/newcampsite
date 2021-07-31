41468535f399f51f38f8ad1140e26ad37d75d032
Zach Gregory <zachgregorync@gmail.com>
2021-07-31 12:12:57 -0400

    added jquery on modals and updates scss modal header

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,11 @@ $(function() {
         }
     });
 });
+
+$("#reserveButton").on('click', function() {
+    $('#reserveModal').modal('show');
+});
+
+$("#loginButton").on('click', function() {
+    $('#loginModal').modal('show');
+});
